e39d6d5743967c59af33b3ae34074267614f12a7
dayva001 <dariusayvazian@yahoo.com>
2024-10-18 14:21:15 -0700

    added pickup

--- Group 14 game/Assets/Scripts/PickupItem.cs
+++ Group 14 game/Assets/Scripts/PickupItem.cs
@@ -6,6 +6,18 @@ public class PickupItem : MonoBehaviour
 {
     bool playerInRange = false;
     Collider player = null;
+    //adjust this to change how high it goes
+    public float height = 1f;
+    public float rotateSpeed = 3f;
+    private float timeOffset;
+    public GameObject pickupUI;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeOffset = Random.Range(-1f, 1f);
+        pickupUI.SetActive(false);
+    }
     // Update is called once per frame
     void Update()
     {
@@ -13,20 +25,44 @@ public class PickupItem : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
-                player.gameObject.GetComponent<Animator>().SetTrigger("Grab");
+                EquipItem();
             }
         }
     }
 
+     void FixedUpdate()
+    {
+ // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
+        transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime*rotateSpeed);
+         //get the objects current position and put it in a variable so we can access it later with less code
+        Vector3 pos = transform.position;
+        
+        //calculate what the new Y position will be
+        float newY = pos.y + Mathf.Sin((Time.time+timeOffset))*(height/180);
+        //set the object's Y to the new calculated Y
+        transform.position = new Vector3(pos.x, newY, pos.z);
+
+    }
+
+    private void EquipItem()
+    {
+        player.gameObject.GetComponent<Animator>().SetTrigger("Grab");
+    }
     
     private void OnTriggerEnter(Collider other)
     {
         player = other;
-        playerInRange = true;   
+        playerInRange = true;
+        pickupUI.SetActive(true);
+        
     }
     private void OnTriggerExit(Collider other)
     {
         player = null;
         playerInRange = false;
+        pickupUI.SetActive(false);
     }
+
 }
+
+ 
