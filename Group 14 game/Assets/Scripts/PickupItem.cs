8711c32f88fdfed165fbebff829858825da87875
dayva001 <dariusayvazian@yahoo.com>
2024-10-18 19:33:54 -0700

    added broadsword pickup

--- Group 14 game/Assets/Scripts/PickupItem.cs
+++ Group 14 game/Assets/Scripts/PickupItem.cs
@@ -11,7 +11,10 @@ public class PickupItem : MonoBehaviour
     public float rotateSpeed = 3f;
     private float timeOffset;
     public GameObject pickupUI;
-    
+    public GameObject equipment;
+    public AnimatorOverrideController newAnimations;
+    private bool equipped = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,21 +35,22 @@ public class PickupItem : MonoBehaviour
 
      void FixedUpdate()
     {
- // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
-        transform.Rotate (new Vector3 (0, 30, 0) * Time.deltaTime*rotateSpeed);
-         //get the objects current position and put it in a variable so we can access it later with less code
-        Vector3 pos = transform.position;
-        
-        //calculate what the new Y position will be
-        float newY = pos.y + Mathf.Sin((Time.time+timeOffset))*(height/180);
-        //set the object's Y to the new calculated Y
-        transform.position = new Vector3(pos.x, newY, pos.z);
-
+        Rotate();
     }
 
     private void EquipItem()
     {
+        equipped = true;
+        pickupUI.SetActive(false);
         player.gameObject.GetComponent<Animator>().SetTrigger("Grab");
+        GameObject parent = GameObject.Find("WeaponHolder");
+        transform.SetParent(parent.transform);
+        transform.localPosition = Vector3.zero;
+        transform.localRotation = Quaternion.Euler(Vector3.zero);
+        transform.localScale = Vector3.one;
+        //Instantiate(equipment, parent.transform);
+        player.gameObject.GetComponent<Animator>().runtimeAnimatorController = newAnimations;
+        //Destroy(this.gameObject.transform.parent.gameObject);
     }
     
     private void OnTriggerEnter(Collider other)
@@ -61,6 +65,22 @@ public class PickupItem : MonoBehaviour
         player = null;
         playerInRange = false;
         pickupUI.SetActive(false);
+    }
+
+    private void Rotate()
+    {
+        if (!equipped)
+        {
+            // Rotate the object on X, Y, and Z axes by specified amounts, adjusted for frame rate.
+            transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime * rotateSpeed);
+            //get the objects current position and put it in a variable so we can access it later with less code
+            Vector3 pos = transform.position;
+
+            //calculate what the new Y position will be
+            float newY = pos.y + Mathf.Sin((Time.time + timeOffset)) * (height / 180);
+            //set the object's Y to the new calculated Y
+            transform.position = new Vector3(pos.x, newY, pos.z);
+        }
     }
 
 }
