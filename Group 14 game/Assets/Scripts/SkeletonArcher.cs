e02c8d546f2b48840dcb69aa225918353c45e6f7
dayva001 <dariusayvazian@yahoo.com>
2024-11-03 20:54:47 -0800

    added skeleton archer enemy

---
+++ Group 14 game/Assets/Scripts/SkeletonArcher.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+
+public class SkeletonArcher : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public List<Transform> player;
+    public LayerMask whatIsGround, whatIsPlayer;
+    //Patroling
+    public Vector3 walkPoint;
+    public Vector3 walkDirection;
+    public float walkPointRange;
+    public bool walkPointSet;
+    private Animator animator;
+    public GameObject firingLocation;
+    //Attacking
+    public float timeBetweenAttacks;
+    bool alreadyAttacked;
+    public GameObject projectile;
+
+    //States
+    public float sightRange, attackRange;
+    public bool playerInSightRange, playerInAttackRange;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Awake()
+    {
+
+        player = GameObject.FindGameObjectsWithTag("Player").Select(go => go.transform).ToList();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check for sight and attack range
+        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
+        playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
+        if (!playerInSightRange && !playerInAttackRange) Patroling();
+        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
+        if (playerInSightRange && playerInAttackRange) AttackPlayer();
+    }
+
+    void Patroling()
+    {
+        if (!walkPointSet) SearchWalkPoint();
+        if (walkPointSet)
+            agent.SetDestination(walkPoint);
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1f)
+            walkPointSet = false;
+
+    }
+
+    private void SearchWalkPoint()
+    {
+        //calculate random point in range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+            walkPointSet = true;
+    }
+    void ChasePlayer()
+    {
+        Transform nearestPlayer = NearestPlayer();
+        agent.SetDestination(nearestPlayer.position);
+        Vector3 direction = (nearestPlayer.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+    private void AttackPlayer()
+    {
+        agent.SetDestination(transform.position);
+
+        transform.LookAt(NearestPlayer().position);
+
+        if (!alreadyAttacked)
+        {
+            //attack code here
+            animator.SetTrigger("Attacking");
+            StartCoroutine(InstantiateProjectileAfterAnimation());
+            alreadyAttacked = true;
+            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+        }
+    }
+
+    private IEnumerator InstantiateProjectileAfterAnimation()
+    {
+        agent.isStopped = true;
+        yield return new WaitForSeconds(animator.runtimeAnimatorController.animationClips[1].length);
+        Quaternion arrowAngle = Quaternion.LookRotation((NearestPlayer().transform.position + new Vector3(0f,0.5f,0f)) - firingLocation.GetComponent<Renderer>().bounds.center);
+        GameObject arrow = Instantiate(projectile, firingLocation.GetComponent<Renderer>().bounds.center, arrowAngle);
+    }
+    private Transform NearestPlayer()
+    {
+        Transform nearestPlayer = player[0];
+        for (int i = 0; i < player.Count; i++)
+        {
+            if (Vector3.Distance(transform.position, player[i].position) < Vector3.Distance(transform.position, nearestPlayer.position))
+                nearestPlayer=player[i];
+        }
+        return nearestPlayer;
+    }
+    private void ResetAttack()
+    {
+        alreadyAttacked = false;
+        agent.isStopped = false;
+    }
+}
